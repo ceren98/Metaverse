ecc571b8ed6a65405daac418b909be606271c474
ceren98 <cereneycan98@gmail.com>
2022-12-13 08:16:26 +0300

    Metaverse

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+
+[System.Serializable] public class DefaultRoom
+{
+    public string Name;
+    public int sceneIndex;
+    public int maxPlayer;
+}
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public List<DefaultRoom> defaultRooms;
+
+    public GameObject roomUI;
+    public void ConnectToServer()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("Try Connect to server..");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Server");
+        base.OnConnectedToMaster();
+
+        PhotonNetwork.JoinLobby();
+
+       
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        Debug.Log("WE JOINED THE LOBBY");
+        roomUI.SetActive(true);
+        //GameObject.Find("Rooms UI").SetActive(true);
+    }
+
+    public void InitiliazeRoom(int defaultRoomIndex)
+    {
+        DefaultRoom roomSettings = defaultRooms[defaultRoomIndex];
+
+        //LOAD SCENE
+        PhotonNetwork.LoadLevel(roomSettings.sceneIndex);
+
+        //CREATE THE ROOM
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = (byte)roomSettings.maxPlayer;
+        roomOptions.IsVisible = true;
+        roomOptions.IsOpen = true;
+
+        PhotonNetwork.JoinOrCreateRoom(roomSettings.Name, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined a room");
+        base.OnJoinedRoom();
+    }
+
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("A new player joined the room");
+        base.OnPlayerEnteredRoom(newPlayer);
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using Photon.Pun;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class NetworkPlayer : MonoBehaviour
+{
+
+    public Transform head;
+    public Transform leftHand;
+    public Transform rightHand;
+    private PhotonView photonView;
+
+    
+    public Animator leftHandAnimator;
+    public Animator rightHandAnimator;
+
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;
+
+  
+  
+    // Start is called before the first frame update
+    public void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+
+
+         XRRig rig = FindObjectOfType<XRRig>();
+         headRig = rig.transform.Find("Camera Offset/Main Camera");
+         leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+         rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+
+        if(photonView.IsMine)
+        {
+
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+               item.enabled = false;
+            }
+        
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (photonView.IsMine)
+        {
+
+          
+
+         /*   MapPosition(head, XRNode.Head);
+            MapPosition(leftHand, XRNode.LeftHand);
+            MapPosition(rightHand, XRNode.RightHand);*/
+
+            MapPosition(head, headRig);
+            MapPosition(leftHand, leftHandRig);
+            MapPosition(rightHand, rightHandRig);
+
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand),leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+
+        }
+
+
+      
+    }
+
+    void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+        if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    void MapPosition(Transform target, Transform rigTransform) // (,XRNode node) 
+                                                                   //target.position= position, target.rot=rot
+    {
+        // InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.devicePosition, out Vector3 position);
+        //InputDevices.GetDeviceAtXRNode(node).TryGetFeatureValue(CommonUsages.deviceRotation, out Quaternion rotation);
+
+       // target.position = position;
+        //target.rotation = rotation;
+
+    target.position = rigTransform.position;
+    target.rotation = rigTransform.rotation;
+
+    }
+
+}
